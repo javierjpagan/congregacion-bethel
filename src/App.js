6360b01aec25bcee03aa0c6f21d5ac7619d39390
Javier <javierjpagan@gmail.com>
2020-02-10 23:31:55 -0500

    Styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 
 import Navbar from "./components/navbar/";
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,20 @@
 import React, { Component, Fragment } from "react";
 
-import NavStyle from "./components/styled/NavStyle";
+import styled from "styled-components";
+
+const NavStyle = styled.div`
+
+color: black;
+    padding: 10px 10px;
+    text-transform: uppercase;
+    letter-spacing: .05em;
+    font-size: 15px;
+    text-decoration: none !important; 
+    display: inline-block;
+    float: none;
+    font-family: "Work Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    box-sizing: border-box;
+`;
 
 
 class Navbar extends Component {
