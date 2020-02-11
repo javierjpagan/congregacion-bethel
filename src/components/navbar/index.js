d4d8c5c28b12c7bdc4e8e8bdcea77600955d0ca6
Javier <javierjpagan@gmail.com>
2020-02-11 16:28:50 -0500

    Formatted Home Page

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,20 +1,23 @@
 import React, { Component, Fragment } from "react";
+import './style.css';
 
 import styled from "styled-components";
 
-const NavStyle = styled.div`
+// const NavStyle = styled.div`
 
-color: black;
-    padding: 10px 10px;
-    text-transform: uppercase;
-    letter-spacing: .05em;
-    font-size: 15px;
-    text-decoration: none !important; 
-    display: inline-block;
-    float: none;
-    font-family: "Work Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-    box-sizing: border-box;
-`;
+//     color: black;
+//     padding: 10px 10px;
+//     text-transform: uppercase;
+//     letter-spacing: .05em;
+//     font-size: 15px;
+//     text-decoration: none !important; 
+//     display: inline-block;
+//     float: none;
+//     font-family: "Work Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+//     box-sizing: border-box;
+//     margin-left: auto;
+//     margin-right: auto;
+// `;
 
 
 class Navbar extends Component {
@@ -24,17 +27,19 @@ class Navbar extends Component {
 
 
 
-
+            // <NavStyle>
+            <div className="row align-items-center justify-content-center">
+            <div className="col-md-7 text-center" data-aos="fade">
             <div className="main-navbar">
-            <NavStyle>
                             <li><a href="/inicio">Inicio</a></li>
                             <li><a href="/conocenos">Conócenos</a></li>
                             <li><a href="/servicios">Servicios & Horarios</a></li>
                             <li><a href="/donativos">Donativos</a></li>
                             <li><a href="/contactanos">Contáctanos</a></li>
-                            </NavStyle>
-
                         </div>
+                        </div>
+                        </div>
+                                                    // </NavStyle>
 
 
         )
--- src/components/pages/inicio/index.js
+++ src/components/pages/inicio/index.js
@@ -7,11 +7,11 @@ import {
 export default class Inicio extends Component {
     render() {
         return(
-            <div class="site-blocks-cover" data-aos="fade" data-stellar-background-ratio="0.5">
+            <div className="site-blocks-cover" data-aos="fade" data-stellar-background-ratio="0.5">
+                           <div className="row align-items-center justify-content-center">
+                <div className="col-md-7 text-center" data-aos="fade">
              <img className="logo-home" src="/images/logo-bethel.jpg"></img>
-            <div class="container">
-              <div class="row align-items-center justify-content-center">
-                <div class="col-md-7 text-center" data-aos="fade">
+            <div className="container">
                   <h1><strong>CONGREGACIÓN MERSIÁNICA BETH-EL</strong></h1>
                   <h1>(Kehilah Mashiajit Beth-El)</h1>
                 </div>
