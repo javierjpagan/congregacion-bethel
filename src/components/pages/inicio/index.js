3bba3483e5a6836e74e284ed875f3bb61eda743c
Javier <javierjpagan@gmail.com>
2020-02-20 16:45:07 -0500

    Fixed HomePage picture

--- src/components/pages/inicio/index.js
+++ src/components/pages/inicio/index.js
@@ -8,7 +8,7 @@ export default class Inicio extends Component {
             <div className="site-blocks-cover" data-aos="fade" data-stellar-background-ratio="0.5">
                            <div className="row align-items-center justify-content-center">
                 <div className="col-md-7 text-center" data-aos="fade">
-             <img className="logo-home" src="/images/logo-bethel.jpg"></img>
+             <img className="logo-home" src="https://www.pastepic.xyz/images/2020/02/20/logo-bethel42dc84c6e31ecad9.jpg" alt="logo-bethel42dc84c6e31ecad9.jpg" border="0" ></img>
             <div className="container">
             <div className="InicioHead">
                   <h1><strong>CONGREGACIÓN MERSIÁNICA BETH-EL (TESTING)</strong></h1>
