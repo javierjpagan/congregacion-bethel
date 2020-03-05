447e48928c0350d0f8a5a7062f77a69c3585e2c8
Javier <javierjpagan@gmail.com>
2020-03-05 16:51:43 -0500

    Finished setting up Google Maps

--- src/components/pages/contactanos/index.js
+++ src/components/pages/contactanos/index.js
@@ -1,55 +1,96 @@
 import React, { Component } from "react";
-import GoogleMapReact from 'google-map-react'; // OUT
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import './style.css';
 
-const mapStyles = {
-    width: '50%',
-    height: '50%',
-    display: 'block',
-    margin: '0px auto',
-    position: 'relative'
+const mapContainerStyles = {
+    width: '100%',
+    height: '500px',
+    position: 'relative',
 
   };
 
 class Contactanos extends Component {
 
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+onClose = props => {
+  if (this.state.showingInfoWindow) {
+    this.setState({
+      showingInfoWindow: false,
+      activeMarker: null
+    });
+  }
+};
+
     render() {
         return(
 
-<div className="ContBody">
+<div className="ContBody container-fluid">
             <div className="row align-items-center justify-content-center">
             <div className="col-md-7 text-center" data-aos="fade">
             <div className="ContHead">
 <h2><strong>CONTÁCTANOS</strong></h2>
 </div>
             </div>
+            </div>
             
 
+            <div className="row align-items-center justify-content-center">
 <h2>Dirección Postal: HC 1 Box 29030 PMB 602, Caguas, PR 00725-8900</h2> 
+</div>
+
+<div className="row align-items-center justify-content-center">
 
 <h2>{`Dirección Física: Carretera #1 Km. 27.0 (Frente al Parque de Soccer), 
 Sector Quebrada Arenas, Bo. Río Cañas, Caguas, Puerto Rico`}</h2> 
+</div>
 
-
-      <Map className="GoogMaps"
-          google={this.props.google}
-          zoom={13}
-          style={mapStyles}
-          initialCenter={{ lat: 18.3017, lng: -66.0525}} 
+<div className="GoogMaps" style={mapContainerStyles}>
+            <Map
+              google={this.props.google}
+              zoom={13}
+              initialCenter={{ lat: 18.3017, lng: -66.0525 }}
+            >
+                          <Marker 
+                          position={{ lat: 18.301756, lng: -66.052734}}
+          onClick={this.onMarkerClick}
+          name={'Beth-El Congregación'}
         />
-        
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+                          </Map>
+          </div>
 
 
-
-
-
-
+        <div className="row align-items-center justify-content-center">
 <h2>Conexión con Google Map para que obtengan la dirección para llegar</h2>  
 
-<h2>Teléfono: (787) 486-3507</h2>   
 
+<div className="row align-items-center justify-content-center">
+<h2>Teléfono: (787) 486-3507</h2>   
+</div>
+
+<div className="row align-items-center justify-content-center">
 <h2>Email: kehilah_beth_el@yahoo.com</h2> 
+</div>
                         </div>
                         </div>
 
