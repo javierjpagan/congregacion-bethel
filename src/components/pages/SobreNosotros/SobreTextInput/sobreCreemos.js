fb9c096b090dfa0e7f687af3de7df8c6fadd99eb
davidpom <davidpom@gmail.com>
2020-03-12 16:13:44 -0400

    test

--- src/components/pages/SobreNosotros/SobreTextInput/sobreCreemos.js
+++ src/components/pages/SobreNosotros/SobreTextInput/sobreCreemos.js
@@ -1,46 +1,8 @@
-import React, { Component } from "react";
-import "./style.css";
+const sobreCreemos = 
+[
+  "	1. Creemos en un sólo Elohim y en un sólo mediador entre Él y los hombres, Yahshúa Ha’Mashiaj. 2. Creemos que Yahshúa es el Hijo de Elohim y que en Él tenemos redención en Su sangre y justificación por medio de la fe, jamás por las obras. 3. Creemos que la Biblia (desde Génesis hasta Revelación) es Palabra de Elohim y regla de fe. 4. Creemos que la Toráh (Ley) contiene las instrucciones para una vida justa y fructífera por medio de los mandamientos y principios eternos en ella desglosados puesto que Yahshúa no vino a abrogar la Torá sino a interpretarla en su sentido más completo (Matityahu / Mateo 5:17-19). 5. Creemos que Elohim ha impartido Su Ruaj HaKodesh (Espíritu de Santidad) a los hombres y que éste se manifiesta por medio de Sus dones. 6. Creemos en la resurrección de los muertos y que Yahshúa vendrá por Su Novia. Creemos que Él retornará a la tierra en gloria para establecer el Reino Milenial y sentarse en el Trono de David. 7. Creemos en la inmersión en las aguas luego de haber creído en el Mesías Yahshúa. 8. Creemos que el creyente en Yahshúa debe vivir una vida santa en conformidad a las Escrituras y dar fruto de verdadero arrepentimiento. 9. Creemos en la adoración espontánea levantando la voz, las manos y con danza que exprese el agradecimiento y el amor a Elohim. 10. Creemos en la unidad del Cuerpo del Mesías. 11. Creemos que Elohim está restaurando a Israel según dicho por los profetas y que los que perdieron su identidad israelita por causa de la dispersión de Israel por el mundo están siendo llamados por Elohim a regresar a sus raíces hebreas y a recobrar su identidad y cultura israelita."
+]
+;
 
-class sobreCreemos extends Component {
-  render() {
-    return (
-      <div className="sobreCollapText">
-        <h2>
-          1. Creemos en un sólo Elohim y en un sólo mediador entre Él y los
-          hombres, Yahshúa Ha’Mashiaj. <br></br>
-          2. Creemos que Yahshúa es el Hijo de Elohim y que en Él tenemos
-          redención en Su sangre y justificación por medio de la fe, jamás por
-          las obras. <br></br>
-          3. Creemos que la Biblia (desde Génesis hasta Revelación) es Palabra
-          de Elohim y regla de fe. <br></br>
-          4. Creemos que la Toráh (Ley) contiene las instrucciones para una vida
-          justa y fructífera por medio de los mandamientos y principios eternos
-          en ella desglosados puesto que Yahshúa no vino a abrogar la Torá sino
-          a interpretarla en su sentido más completo (Matityahu / Mateo
-          5:17-19). <br></br>
-          5. Creemos que Elohim ha impartido Su Ruaj HaKodesh (Espíritu de
-          Santidad) a los hombres y que éste se manifiesta por medio de Sus
-          dones. <br></br>
-          6. Creemos en la resurrección de los muertos y que Yahshúa vendrá por
-          Su Novia. Creemos que Él retornará a la tierra en gloria para
-          establecer el Reino Milenial y sentarse en el Trono de David.{" "}
-          <br></br>
-          7. Creemos en la inmersión en las aguas luego de haber creído en el
-          Mesías Yahshúa. <br></br>
-          8. Creemos que el creyente en Yahshúa debe vivir una vida santa en
-          conformidad a las Escrituras y dar fruto de verdadero arrepentimiento.{" "}
-          <br></br>
-          9. Creemos en la adoración espontánea levantando la voz, las manos y
-          con danza que exprese el agradecimiento y el amor a Elohim. <br></br>
-          10. Creemos en la unidad del Cuerpo del Mesías. 11. Creemos que Elohim
-          está restaurando a Israel según dicho por los profetas y que los que
-          perdieron su identidad israelita por causa de la dispersión de Israel
-          por el mundo están siendo llamados por Elohim a regresar a sus raíces
-          hebreas y a recobrar su identidad y cultura israelita.
-        </h2>
-      </div>
-    );
-  }
-}
 
 export default sobreCreemos;
--- src/components/pages/SobreNosotros/SobreTextInput/sobreQuienes.js
+++ src/components/pages/SobreNosotros/SobreTextInput/sobreQuienes.js
@@ -1,31 +1,6 @@
-import React, { Component } from "react";
-import './style.css';
-
-class sobreQuienes extends Component {
-    render() {
-        return(
-
-
-
-
-<div className="sobreCollapText">
-<h2 className="sobreQuien"><strong>Bienvenidos a la CONGREGACIÓN MESIÁNICA BETH-EL De Caguas Puerto Rico </strong></h2>
-
-         
-            
-<div className="QuienesSomosText">
-<h2>Yeshayahu / Isaísas 11:12, registra:  “Y levantarás pendón a las naciones y juntarás los desterrados de Yisrael y reunirá los esparcidos de Yehudá de los cuatro confines de la tierra.” </h2> 
-
-
-<h2>La Congregación Mesiánica Beth-El se funda en el año 1999, en respuesta al proceso iniciado por el Eterno de restaurar la identidad israelita y hacer retornar a casa las Ovejas perdidas de la Casa de Israel y a los esparcidos de la Casa de Judá.  Al caducar la fecha del castigo correspondiente sobre cada uno de los Reinos (Reino del Norte y Reino del Sur), como a muchos otros, el Eterno comenzó a revelar Su plan de restauración dejándonos saber que por virtud del pecado de nuestros ancestros perdimos la identidad israelita siendo descendientes de Avraham, Yitzjak y Yaacov. </h2> 
-<h2>Al descubrir el proceso acontecido a través del tiempo de desconexión de la Toráh así como de la cultura y tradiciones de nuestro Pueblo Israel, en Su Misericordia el Eterno ha venido restaurando el entendimiento y conocimiento de Su Toráh y nos ha permitido recuperar la identidad y cultura israelita que habíamos perdido.  El entender y descubrir que nuestro Mesías Yahshúa no abrogó ni anuló la Toráh ni los Profetas, ni ninguno de los mandamientos ordenados por el Eterno en Su Toráh (Matituyahu 5:17), sino más bien sus palabras y acciones muestran y revelan el espíritu correcto de ello, nos condujo a estudiar y conocer la Toráh del Eterno y sus mandamientos desde la perspectiva hebrea en la que fue dada y poco a poco hemos venido obedeciendo y aplicando sus mandamientos.</h2> 
-<h2>Somos observantes del Shabbat, Rosh Jodesh y las fiestas bíblicas descritas en Vayikrá /Levítico 23, además de las fiestas judías tradicionales y las ceremonias del ciclo de vida judía como bodas, bar/bat mitzvah, berit miláh, etc. </h2> 
-<h2>La Congregación Mesiánica Beth-El se encuentra bajo la cobertura de Alliance Shomer Yisrael, Inc.  (ASY) </h2>                   
-                        </div>
-                        </div>
-
-        )
-    }
-}
-
-export default (sobreQuienes);
+const sobreQuienes = 
+[
+"Bienvenidos a la CONGREGACIÓN MESIÁNICA BETH-EL De Caguas Puerto Rico                                                                    Yeshayahu / Isaísas 11:12, registra:  'Y levantarás pendón a las naciones y juntarás los desterrados de Yisrael y reunirá los esparcidos de Yehudá de los cuatro confines de la tierra.' La Congregación Mesiánica Beth-El se funda en el año 1999, en respuesta al proceso iniciado por el Eterno de restaurar la identidad israelita y hacer retornar a casa las Ovejas perdidas de la Casa de Israel y a los esparcidos de la Casa de Judá. Al caducar la fecha del castigo correspondiente sobre cada uno de los Reinos (Reino del Norte y Reino del Sur), como a muchos otros, el Eterno comenzó a revelar Su plan de restauración dejándonos saber que por virtud del pecado de nuestros ancestros perdimos la identidad israelita siendo descendientes de Avraham, Yitzjak y Yaacov. Al descubrir el proceso acontecido a través del tiempo de desconexión de la Toráh así como de la cultura y tradiciones de nuestro Pueblo Israel, en Su Misericordia el Eterno ha venido restaurando el entendimiento y conocimiento de Su Toráh y nos ha permitido recuperar la identidad y cultura israelita que habíamos perdido. El entender y descubrir que nuestro Mesías Yahshúa no abrogó ni anuló la Toráh ni los Profetas, ni ninguno de los mandamientos ordenados por el Eterno en Su Toráh (Matituyahu 5:17), sino más bien sus palabras y acciones muestran y revelan el espíritu correcto de ello, nos condujo a estudiar y conocer la Toráh del Eterno y sus mandamientos desde la perspectiva hebrea en la que fue dada y poco a poco hemos venido obedeciendo y aplicando sus mandamientos. Somos observantes del Shabbat, Rosh Jodesh y las fiestas bíblicas descritas en Vayikrá /Levítico 23, además de las fiestas judías tradicionales y las ceremonias del ciclo de vida judía como bodas, bar/bat mitzvah, berit miláh, etc.La Congregación Mesiánica Beth-El se encuentra bajo la cobertura de Alliance Shomer Yisrael, Inc.  (ASY)"                 
+]
+;
+export default sobreQuienes;
